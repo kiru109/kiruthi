3818c437538e9a0e856573d3b60b6c94ec701eb1
kiruthikka <kiruthikkamuthuraman@gmail.com>
2024-07-29 10:05:35 +0530

    first commit

---
+++ src/main/java/com/kiruthi/jenkinskiru/model/testsample.java
@@ -0,0 +1,12 @@
+package com.kiruthi.jenkinskiru.model;
+
+public class testsample {
+	  public static int CompareString( String str1, String str2 )
+	    {
+	        if(str1.equals(str2))
+	        	return 1;
+	        else
+	        	return 0;
+	    }
+	    
+}
